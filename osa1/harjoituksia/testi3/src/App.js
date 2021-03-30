d6078ef37c759a208590818170ee592978b821dd
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-03-30 19:56:15 +0300

    harjoitukset tehty osalta 1

--- osa1/harjoituksia/testi3/src/App.js
+++ osa1/harjoituksia/testi3/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
+const History = ({ allClicks }) => {
+  if(allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      button press history: {allClicks.join(' ')}
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const Display = (props) => <div>{props.value}</div> 
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  }
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='Left' />
+        <Button handleClick={handleRightClick} text='Right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+
+      <div>
+        <Display value={value} />
+        <Button handleClick={setToValue(1000)} text='thousand' />
+        <Button handleClick={setToValue(0)} text='reset' />
+        <Button handleClick={setToValue(value + 1)} text='increasement' />
+      </div>
+    </div>
+  )
+}
+
+export default App
