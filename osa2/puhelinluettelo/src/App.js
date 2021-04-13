da921e5311464bbc18bfb5e33a86259fe9ff6f20
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-04-13 19:09:32 +0300

    2.8 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,17 +5,20 @@ const App = () => {
     { name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      number: newNumber,
     }
 
-    if (!(persons.find(p => p.name === newName))){
+    if (!(persons.find(p => p.name === newName))) {
       setPersons(persons.concat(personObject))
       setNewName('')
-    }else{
+      setNewNumber('')
+    } else {
       window.alert(`${newName} is already added to phonebook`)
     }
   }
@@ -24,22 +27,26 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
 
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
-          <p key={person.name}> {person.name}
+          <p key={person.name}> {person.name} {person.number}
           </p>)}
       </div>
     </div>
